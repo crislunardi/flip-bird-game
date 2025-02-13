ffb253d9b5553fc6d4c43b7674b3dbc6bae8cb63
Cristian Lunardi <c.lunarditi@gmail.com>
2025-02-13 19:59:54 -0300

    barriers animations

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function createElement(tagName, className) {
     const elem = document.createElement(tagName);
     elem.className = className;
     return elem;
-}
+};
 
 function CreateBarrier(reverse = false) {
     this.element = createElement('div', 'barrier');
@@ -14,7 +14,7 @@ function CreateBarrier(reverse = false) {
     this.element.appendChild(reverse ? border : body);
 
     this.setHeight = height => (body.style.height = `${height}px`);
-}
+};
 
 function PairOfBarriers(height, opening, x) {
     this.element = createElement('div', 'pair-of-barriers');
@@ -39,7 +39,39 @@ function PairOfBarriers(height, opening, x) {
 
     this.drawOpening();
     this.setX(x);
-}
+};
 
-const b = new PairOfBarriers(700, 200, 400);
-document.querySelector('[wm-flappy]').appendChild(b.element);
+function Barriers(height, width, opening, space, pointNotify) {
+    this.pairs = [
+        new PairOfBarriers(height, opening, width),
+        new PairOfBarriers(height, opening, width + space),
+        new PairOfBarriers(height, opening, width + space * 2),
+        new PairOfBarriers(height, opening, width + space * 3)
+    ]
+
+    const displacement = 3;
+    this.animate = () => {
+        this.pairs.forEach(pair => {
+            pair.setX(pair.getX() - displacement);
+
+            if (pair.getX() < -pair.getWidth()) {
+                pair.setX(pair.getX() + space * this.pairs.length);
+                pair.drawOpening();
+            }
+
+            const middle = width / 2;
+            const crossedMiddle = pair.getX() + displacement >= middle && pair.getX() < middle;
+
+            if (crossedMiddle) {
+                pointNotify();   
+            }
+        });
+    }
+};
+
+const barriers = new Barriers(700, 1200, 200, 400);
+const gameArea = document.querySelector('[wm-flappy]');
+barriers.pairs.forEach(pair => gameArea.appendChild(pair.element));
+setInterval(() => {
+    barriers.animate();
+}, 20);
